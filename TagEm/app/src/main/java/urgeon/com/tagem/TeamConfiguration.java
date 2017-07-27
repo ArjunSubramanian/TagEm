1a65188d1bbe79189c5275a8fefb9f4fcbeacfd3
Arjun Subramanian <arjun.subramanian@outlook.com>
2017-07-26 22:29:32 -0400

    Team Configuration Page

---
+++ TagEm/app/src/main/java/urgeon/com/tagem/TeamConfiguration.java
@@ -0,0 +1,13 @@
+package urgeon.com.tagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamConfiguration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_configuration);
+    }
+}
